839feb4d3cfef51338869aff33a8885aa3a504ec
Simon <89594899+Ssenseii@users.noreply.github.com>
2024-10-17 19:57:40 +0100

    pushing the app

---
+++ lib/my_test_app_web/components/layouts.ex
@@ -0,0 +1,14 @@
+defmodule MyTestAppWeb.Layouts do
+  @moduledoc """
+  This module holds different layouts used by your application.
+
+  See the `layouts` directory for all templates available.
+  The "root" layout is a skeleton rendered as part of the
+  application router. The "app" layout is set as the default
+  layout on both `use MyTestAppWeb, :controller` and
+  `use MyTestAppWeb, :live_view`.
+  """
+  use MyTestAppWeb, :html
+
+  embed_templates "layouts/*"
+end
